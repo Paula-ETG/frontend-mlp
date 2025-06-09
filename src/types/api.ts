496d1162a31d021e394a007ddd0a56fec478603a
Paula-ETG <paula.yap@everythingtogain.com>
2025-06-10 05:18:48 +0800

    feat: added new types

--- src/types/api.ts
+++ src/types/api.ts
@@ -1,3 +1,24 @@
+export type Base = {
+  id: string;
+  created_at: string;
+};
+
+export type SenderType = "assistant" | "user";
+
+export type User = {
+  email: string;
+  id: string;
+  role: string;
+  organization: string;
+  profile: Profile;
+};
+
+export type Profile = {
+  first_name?: string;
+  last_name?: string;
+  nickname?: string;
+};
+
 export type Login = {
   access_token: string;
   api_key: string;
@@ -20,6 +41,58 @@ export type Session = {
   created_at: string;
   user_id: string;
   organization_id: string;
+  assistant_id: string;
+  account_id: string;
+};
+
+export type Tool = {
+  name: string;
+  type: string;
+};
+
+export type ToolConfig = {
+  tools: Tool[];
+};
+
+export type Assistant = {
+  name: string;
+  internal_name: string;
+  description: string;
+  tool_config: ToolConfig;
+  testing: boolean;
+  version: string;
+  developer_prompt: string;
+  model: string;
+  id: string;
+  created_at: string;
+};
+
+type MessageInput = {
+  content: string;
+};
+
+type MessageOutputContent = {
+  text: string;
+  type: string;
+};
+
+type MessageOutput = {
+  id: string;
+  content: MessageOutputContent[];
+  role: string;
+  status: string;
+  type: string;
+};
+
+export type Messages = {
+  id: string;
+  output?: MessageOutput;
+  input?: MessageInput;
+  sender: SenderType;
+  created_at: string;
+  user_id: string;
+  organization_id: string;
+  session_id: string;
   assistant_id: string;
   account_id: string;
 };
