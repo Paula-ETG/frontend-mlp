79f142e171d965b4b475bcd9ecde7b76bcd5dcce
Paula-ETG <paula.yap@everythingtogain.com>
2025-06-10 17:01:04 +0800

    feat: added create session mutation on click of a card assistant

--- src/app/routes/chat/new-chat.tsx
+++ src/app/routes/chat/new-chat.tsx
@@ -1,8 +1,29 @@
 import { AssistantCard } from "@/features/chat/components/assistant-card";
 import { useAssistants } from "@/features/chat/api/get-assistants";
+import { useCreateSession } from "@/features/chat/api/create-session";
+import { useNavigate, useParams } from "react-router";
 
 export const NewChat = () => {
+  const navigate = useNavigate();
+  const { accountId } = useParams();
   const { data } = useAssistants({});
+  const createSession = useCreateSession({
+    mutationConfig: {
+      onError: () => {},
+      onSuccess: (data) => {
+        navigate(`/${accountId}/s/${data.id}`);
+      },
+    },
+  });
+
+  const handleClick = (id: string) => {
+    createSession.mutate({
+      title: "test1",
+      summary: "test1",
+      account_id: accountId!,
+      assistant_id: id,
+    });
+  };
 
   return (
     <div className="flex flex-col flex-1 overflow-auto justify-center px-4 items-center">
@@ -14,9 +35,11 @@ export const NewChat = () => {
       <div className="pt-4 grid auto-rows-min gap-4 md:grid-cols-2">
         {data?.map((assistant) => (
           <AssistantCard
+            id={assistant.id}
             title={assistant.name}
             description={assistant.description}
             key={assistant.id}
+            handleClick={handleClick}
           />
         ))}
       </div>
