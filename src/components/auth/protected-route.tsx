08af54fedbb67ee6f40b2652bc7d113404f5cd74
Paula-ETG <paula.yap@everythingtogain.com>
2025-06-18 23:08:39 +0800

    refactor: modified protected route to use useNavigate and use the authState to check for authentication state

--- src/components/auth/protected-route.tsx
+++ src/components/auth/protected-route.tsx
@@ -1,25 +1,28 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
 import { useAuth } from "@/hooks/use-user";
-import { Navigate } from "react-router";
 
 export const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
-  const { isAuthenticated, token } = useAuth();
+  const { authState } = useAuth();
+  const navigate = useNavigate();
 
-  // If no token, redirect to login
-  if (!token) {
-    return <Navigate to="/" replace />;
+  useEffect(() => {
+    if (authState === "unauthenticated") {
+      navigate("/");
+    }
+  }, [authState]);
+
+  if (authState === "authenticated") {
+    return <>{children}</>;
   }
 
-  // If token exists but user is not authenticated yet, show loading or let AuthLoader handle it
-  if (token && !isAuthenticated) {
-    return (
-      <div className="min-h-screen flex items-center justify-center">
-        <div className="text-center">
-          <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900"></div>
-          <p className="mt-4 text-gray-600">Loading...</p>
-        </div>
+  // loading state
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="text-center">
+        <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900"></div>
+        <p className="mt-4 text-gray-600">Loading...</p>
       </div>
-    );
-  }
-
-  return <>{children}</>;
+    </div>
+  );
 };
