9f4c793133a569dca07a305c5f55f5b9a9bf70e2
Paula-ETG <paula.yap@everythingtogain.com>
2025-06-21 01:45:46 +0800

    refactor: removed comments, cleaned code

--- src/app/routes/chat/chat-body.tsx
+++ src/app/routes/chat/chat-body.tsx
@@ -1,17 +1,5 @@
 import type React from "react";
 
 export const ChatBody = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <>
-      <main className="flex flex-col gap-5 h-full mt-10">
-        {children}
-        {/* <div className="bg-white px-4 pb-4 sticky bottom-0 flex flex-col w-full text-center">
-          <ChatInput placeholder="Ask Journey AI" />
-          <p className="text-sm text-gray-500">
-            Journey AI can make mistakes. Check important info.
-          </p>
-        </div> */}
-      </main>
-    </>
-  );
+  return <main className="flex flex-col gap-5 h-full mt-10">{children}</main>;
 };
